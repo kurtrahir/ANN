36a0932938da35110c338f285dcd9c29685115c2
kvr <kurtrahir@gmail.com>
2023-02-21 10:23:08 +0100

    Import new activation functions.

--- ANN/__init__.py
+++ ANN/__init__.py
@@ -1,6 +1,5 @@
-from .activation_functions import Linear, ReLu, Activation
+from .activation_functions import Linear, ReLu, Activation, LeakyReLu, TanH, Sigmoid
 from .loss_functions import Loss, SquareError
 from .neuron import Neuron
 from .layers import Layer, Dense, DenseMatrix, gorlot
 from .network import Network
-from .layers.initializers import gorlot
--- ANN/activation_functions/__init__.py
+++ ANN/activation_functions/__init__.py
@@ -2,3 +2,6 @@
 from .activation import Activation
 from .reLu import ReLu
 from .linear import Linear
+from .leaky_ReLu import LeakyReLu
+from .sigmoid import Sigmoid
+from .tanh import TanH
