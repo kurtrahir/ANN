Fix backpropagation and Adam optimizer. Remove loss from layer and add to optimizer.

ANN/layers/dense.py
@@ -1,20 +1,19 @@
-"""Dense layer implementation ommitting neuron objects
+"""Dense layer implementation
 """
 import numpy as np
 from numpy.typing import NDArray
 from ANN.layers import Layer
 from ANN.layers.initializers import gorlot
 from ANN.activation_functions import Activation
-from ANN.loss_functions import Loss
 
-class DenseMatrix(Layer):
-
+class Dense(Layer):
+    """Implementation of a densely connected layer
+    """
     def __init__(
             self,
             n_inputs : int,
             n_neurons : int,
-            activation : Activation,
-            loss: Loss
+            activation : Activation
         ):
         # Store shape of layer
         self.n_inputs = n_inputs
@@ -35,7 +34,6 @@ class DenseMatrix(Layer):
         self.d_inputs = np.zeros((1,n_inputs))
         # Set activation and loss function
         self.activation_function = activation
-        self.loss_function = loss
         self.linear_combination = np.zeros(np.dot(self.inputs, self.weights).shape)
 
         def forward(inputs : NDArray[np.float32]) -> NDArray[np.float32]:
@@ -45,18 +43,15 @@ class DenseMatrix(Layer):
             return self.outputs
 
         def backward(
-            targets: NDArray[np.float32]
+            error: NDArray[np.float32]
         ):
-            # Get loss derivative with regards to output.
-            d_loss = self.loss_function.backward(self.outputs, targets)
             # Get derivative of outputs with regards to dot product
             d_activation = self.activation_function.backward(self.linear_combination)
             # Derivative of dot product with regards to weights is the inputs.
             # Get derivative of loss with regards to weights:
-            self.d_weights = np.dot(self.inputs.T, d_loss * d_activation)
-
+            self.d_weights = np.dot(self.inputs.T, error * d_activation)
             # Get derivative of output with regards to inputs.
-            self.d_inputs = np.dot(d_loss * d_activation, self.weights[:-1,:].T)
+            self.d_inputs = np.dot(error * d_activation, self.weights[:-1,:].T)
 
             return self.d_inputs
 

ANN/models/model.py
@@ -21,37 +21,44 @@ class Model:
         self.backward = backward
         self.optimizer = optimizer
         self.layers = layers
-        self.history = {"Losses" : {}}
+        self.history = {"training_loss": {}, "validation_loss": {}}
 
     def train(
             self,
-            inputs : NDArray[np.float32],
-            targets : NDArray[np.float32],
+            x : NDArray[np.float32],
+            y : NDArray[np.float32],
             epochs : int,
-            batch_size : int
+            batch_size : int,
+            val_x : NDArray[np.float32] = None,
+            val_y : NDArray[np.float32] = None,
         ):
         """Method to train network on provided dataset."""
-        if inputs.shape[0] != targets.shape[0]:
+        if x.shape[0] != y.shape[0]:
             raise ValueError(
                 f"Mismatched number of samples in inputs and targets: \
-                {inputs.shape[0]} != {targets.shape[0]}"
+                {x.shape[0]} != {y.shape[0]}"
             )
 
-        if inputs.shape[0] % batch_size != 0:
+        if x.shape[0] % batch_size != 0:
             warnings.warn(
                 "Number of samples not evenly divisible by batch size.\
                 Smaller batch will occur."
             )
+            
 
-        loss_function = self.layers[-1].loss_function
         for _ in range(epochs):
             print(f"Epoch : {self.optimizer.epochs+1}")
-            for batch_idx in tqdm(range(batch_size, inputs.shape[0], batch_size)):
+            for batch_idx in tqdm(range(batch_size, x.shape[0], batch_size)):
                 self.backward(
-                    inputs[batch_idx-batch_size:batch_idx],
-                    targets[batch_idx-batch_size:batch_idx]
+                    x[batch_idx-batch_size:batch_idx],
+                    y[batch_idx-batch_size:batch_idx]
                 )
             self.optimizer.epochs += 1
-            loss = loss_function.forward(self.forward(inputs), targets)
-            self.history["Losses"][self.optimizer.epochs] = loss
-            print(f"Loss : {np.mean(np.mean(loss, axis = 1))}")
+            training_loss = self.optimizer.loss.forward(self.forward(x), y)
+            self.history["training_loss"][self.optimizer.epochs] = training_loss
+            print(f"Training Loss : {np.mean(np.mean(training_loss, axis = 1))}")
+            if not val_x is None and not val_y is None:
+                validation_loss = self.optimizer.loss.forward(self.forward(val_x), val_y)
+                self.history["validation_loss"][self.optimizer.epochs] = validation_loss
+                print(
+                    f"Validation Loss : {np.mean(np.mean(validation_loss, axis = 1))}")

ANN/optimizers/adam.py
@@ -3,11 +3,11 @@
 import numpy as np
 from numpy.typing import NDArray
 from ANN.optimizers import Optimizer
-
+from ANN.loss_functions import Loss
 
 class Adam(Optimizer):
-
-    def __init__(self, beta_1 = 0.9, beta_2 = 0.999, learning_rate = 1e-3, epsilon = 1e-15):
+    """Implementation of the Adam optimizer"""
+    def __init__(self, loss : Loss, beta_1 = 0.9, beta_2 = 0.999, learning_rate = 1e-3, epsilon = 1e-15):
 
         self.momentums = {}
         self.beta_1 = beta_1
@@ -29,34 +29,24 @@ class Adam(Optimizer):
 
             n_samples = inputs.shape[0]
             n_inputs = inputs.shape[1]
-            n_layers = len(model.layers)
             gradients =  [
                     np.zeros(layer.weights.shape) for layer in model.layers
                 ]
 
-            for i in range(n_samples):
-                outputs = model.layers[0].forward(
-                    inputs[i].reshape(-1, n_inputs))
-                for i in range(1, len(model.layers)):
-                    outputs = model.layers[i].forward(outputs)
+            for sample_idx in range(n_samples):
+                pred = model.forward(inputs[sample_idx].reshape(-1, n_inputs))
+                temp_t = self.loss.backward(pred, targets[sample_idx])
+                for layer_idx in range(1, len(model.layers)+1):
+                    temp_t = model.layers[-layer_idx].backward(temp_t)
+                    gradients[-layer_idx] += model.layers[-layer_idx].d_weights
 
-                gradients[-1] += self.get_update(
-                    model.layers[-1].backward(targets),
-                    n_layers-1
-                )
-                for i in range(2, len(model.layers)):
-                    gradients[-i] += self.get_update(
-                        model.layers[-i].backward(
-                            gradients[-i+1]
-                        ),
-                        n_layers-i
-                    )
-                
-                for i, layer in enumerate(model.layers):
-                    layer.weights -= self.learning_rate * gradients[i] / n_samples
+            for i, layer in enumerate(model.layers):
+                gradients[i] = self.get_update(gradients[i], i)
+                layer.weights -= self.learning_rate * gradients[i] / n_samples
 
         Optimizer.__init__(
             self,
+            loss=loss,
             backward=backward
         )
 

ANN/optimizers/sgd.py
@@ -1,12 +1,13 @@
 """Stochastic Gradient Descent optimizer"""
 import numpy as np
 from numpy.typing import NDArray
+from ANN.loss_functions.loss import Loss
 from ANN.optimizers import Optimizer
 
 class SGD(Optimizer):
     """Stochastic Gradient Descent Optimizer
     """
-    def __init__(self, learning_rate):
+    def __init__(self, loss: Loss, learning_rate : np.float32):
 
         self.learning_rate = learning_rate
 
@@ -22,16 +23,17 @@ class SGD(Optimizer):
                 ]
 
             for sample_idx in range(n_samples):
-                _ = model.forward(inputs[sample_idx].reshape(1,-1))
-                temp_t = targets[sample_idx]
-                for layer_idx in range(1,len(model.layers)):
+                pred = model.forward(inputs[sample_idx].reshape(1,-1))
+                temp_t = self.loss.backward(pred, targets[sample_idx])
+                for layer_idx in range(1,len(model.layers)+1):
                     temp_t = model.layers[-layer_idx].backward(temp_t)
                     gradients[-layer_idx] += model.layers[-layer_idx].d_weights
 
-            for i,layer in enumerate(model.layers):
-                layer.weights -= self.learning_rate * gradients[i] / n_samples
+            for i,_ in enumerate(model.layers):
+                model.layers[i].weights -= self.learning_rate * gradients[i] / n_samples
 
         Optimizer.__init__(
             self,
+            loss=loss,
             backward=backward
         )